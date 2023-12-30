ef672496d71d14f7a82e9367ed38e9a970037f58
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-31 01:12:20 +0530

    dedasdcolos

--- 07pkginstall.sh
+++ 07pkginstall.sh
@@ -7,19 +7,26 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+validate() {
+    if [ $1 -eq 0]; then
+        echo -e " $R FAILED ...$N  check logs"
+        exit 1
+    else
+        echo -e "$G Successfully $2"
+    fi
+}
 if [ $id -ne 0 ]; then
     echo -e "Dont have permission to do this !! $R Please run this by SUDO access $N"
     exit 1
 else
-    for pkg in $@
+    for pkg in $@;
     do
         yum list installed $pkg &>>$logfile
         if [ $? -eq 0 ]; then
-            echo -e  "$pkg Its Already existed so $Y ....SKIPPED $N"
+            echo -e "$pkg Its Already existed so $Y ....SKIPPED $N"
         else
             yum install $pkg -y &>>$logfile
-            echo  -e "Installed ---- $pkg $G Success $N"
+            validate $? "Installing $pkg"
         fi
     done
 fi
