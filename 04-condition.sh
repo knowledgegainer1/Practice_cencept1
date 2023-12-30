6cbdb6240278d913367a19db0b7bd80350a23cb6
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-30 21:56:21 +0530

    dedasd

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-x=21
+# x=21
 
-if [ $x -gt 20 ]; then
-    echo "$x is grtatedthan 20"
+# if [ $x -gt 20 ]; then
+#     echo "$x is grtatedthan 20"
+# else
+#     echo "$x is less than 20"
+# fi
+
+echo "please enter a number from 0 "
+
+read number
+
+if [ $number % 2 -eq 0 ] ; then
+    echo "this number is prime"
 else
-    echo "$x is less than 20"
+    echo "not prim"
 fi
