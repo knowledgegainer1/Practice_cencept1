f0ec2e55087a1d9b68377f37ac707f1cc4f9767c
knowledgegainer1 <clonemaker74@gmail.com>
2023-12-31 00:07:24 +0530

    dedasdcolours

--- 05logfn.sh
+++ 05logfn.sh
@@ -6,27 +6,25 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-logname="/tmp/$0-$dat.log"  # quotes bcos its string "
+logname="/tmp/$0-$dat.log" # quotes bcos its string "
 validate() {
     if [ $1 -eq 0 ]; then
         echo -e "$2 ....is $G SUCCESS"
     else
-        echo -e  "$2 ...$R FAILED"
+        echo -e "$2 ...$R FAILED"
         exit 1
     fi
 }
 
-
-
 if [ $id -eq 0 ]; then
-    echo -e  "$G SUUCCESS $N welcome root USRE"
+    echo -e "$G SUUCCESS $N welcome root USRE"
 else
     echo -e "$R Error: $N u are not root"
     exit 1
 fi
 
-yum install nginx -y  &>> $logname
+yum install nginx -y &>>$logname
 validate $? "installing nginx"
 
-yum install git -y &>> $logname
+yum install git -y &>>$logname
 validate $? "installing GIT"
