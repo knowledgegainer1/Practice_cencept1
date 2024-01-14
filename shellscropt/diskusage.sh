911cdc72d5a1104c1fb96459bac9667956a5d02c
knowledgegainer1 <clonemaker74@gmail.com>
2024-01-14 14:51:56 +0530

    added

--- shellscropt/diskusage.sh
+++ shellscropt/diskusage.sh
@@ -6,10 +6,12 @@ list=$(df -hT | grep -vE 'tmp|File')
 treshold=1
 message=""
 
-while IFS= read -r line; do
-    usage=$(df -hT | grep -vE 'tmp|File' | awk '{print $6F}' | cut -d % -f1)
-    partition=$(df -hT | grep -vE 'tmp|File' | awk '{print $1F}')
-    if [ $usage -ge $treshold ]; then
+while IFS= read -r line;
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line| awk '{print $1F}')
+    if [ $usage -ge $treshold ]; 
+    then
         message+="high disk usage on $partition :$usage \n"
     fi
 done <<<$list
