2bf92d67251a9be6982e74f0280f3f503cf6e612
knowledgegainer1 <clonemaker74@gmail.com>
2024-01-14 11:56:10 +0530

    added

--- shellscropt/ifs.sh
+++ shellscropt/ifs.sh
@@ -8,7 +8,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [  ! -d $source_dir ]; 
+if [  ! -f $source_dir ]; 
 then
     echo  -e " $R directory not exist $N create it first"
     exit 1  # if you dont keep exit it will run continiously until last to stop it fro running we use exit 
